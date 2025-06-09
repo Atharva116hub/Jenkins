041f1d4c908c9f60a60db7b43da07dd63a7d4321
Atharva Topre <atharva.topre116@gmail.com>
2025-06-09 15:14:08 +0530

    Create pipeline.groovy

---
+++ pipeline.groovy
@@ -0,0 +1,25 @@
+pipeline {
+    agent any
+    stages {
+        stage('git_pull') {
+            steps {
+                echo "pull success"
+            }
+        }
+        stage('build') {
+            steps {
+                 echo "build success"
+            }
+        }
+        stage('test') {
+            steps {
+                echo "test success"
+            }
+        }
+                stage('depoly') {
+            steps {
+                sh 'deploy success'
+            }
+        }
+    }
+}
